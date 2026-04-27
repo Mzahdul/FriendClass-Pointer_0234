73cc556f444cfc5a32bd1fce19ac1c3d8bd5c38b
Mzahdul <seeglas8@gmail.com>
2026-04-27 17:41:47 +0700

    int main

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -3,8 +3,22 @@ using namespace std;
 class mahasiswa{
     public:
     int nim;
-    void shownim(){
+    void showNim(){
         cout<<"no induk = "<<nim<<endl;
     }
 };
+
+int main(){
+    mahasiswa mhs{1}; //object mhs
+    mhs.showNim(); // Member Acces Operator
+    
+    mahasiswa &refMhs = mhs; //pointer reference
+    refMhs.nim = 2; //member acces operator
+    refMhs.showNim(); // Member Acces Operator
+
+    mahasiswa *ptrMhs = &mhs; //pointer
+    ptrMhs->nim = 3; // arrow operator
+    ptrMhs->showNim(); // 
+    return 0;
+}
 
