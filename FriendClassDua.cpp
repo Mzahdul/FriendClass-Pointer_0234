e78e1182c977dc4e469a592e42fab40d98203075
Mzahdul <seeglas8@gmail.com>
2026-04-27 18:26:21 +0700

    class pelajar

--- FriendClassDua.cpp
+++ FriendClassDua.cpp
@@ -3,3 +3,15 @@ using namespace std;
 
 class pelajar;
 class manusia
+{
+public:
+    void showNiliaipelajar(pelajar &x);
+};
+
+class pelajar
+{
+private:
+    int nilai;
+public:
+    pelajar() : nilai(100) {}
+};
