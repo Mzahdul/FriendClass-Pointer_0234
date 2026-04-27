68b0d2bef835982fb8fa4ed1ea8133f334154073
Mzahdul <seeglas8@gmail.com>
2026-04-27 18:16:06 +0700

    void siswa until return

--- FriendClssSatu.cpp
+++ FriendClssSatu.cpp
@@ -22,3 +22,21 @@ public:
     void setId(int pId);
     void display(orang &a);
 };
+
+void siswa::display(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("joko susislo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.display(joko);
+    return 0;
+}
